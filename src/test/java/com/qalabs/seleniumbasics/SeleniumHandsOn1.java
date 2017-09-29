a1ba980dba2a6778d75822aa921d9ab0faed8b4d
Luis Rivas <luis.m.rivas@oracle.com>
2017-09-28 23:29:50 -0500

    Add final version of examples

--- src/test/java/com/qalabs/seleniumbasics/SeleniumHandsOn1.java
+++ src/test/java/com/qalabs/seleniumbasics/SeleniumHandsOn1.java
@@ -8,28 +8,22 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.io.File;
 
-public class WebDriverSetup {
-    public static WebDriver getDriver(String browser) {
-        File rootPath = new File("src/test/resources/lib-thirdparty/driversforwin");
-        if(browser.equals("chrome")) {
-            File chromeFilePath = new File(rootPath, "chromedriver.exe");
-            System.setProperty("webdriver.chrome.driver", chromeFilePath.getPath());
-            return new ChromeDriver();
-        } else if (browser.equals("firefox")) {
-            File firefoxFilePath = new File(rootPath, "geckodriver.exe");
-            System.setProperty("webdriver.gecko.driver", firefoxFilePath.getPath());
-            return new FirefoxDriver();
-        } else {
-            return null;
-        }
-    }
+public class SeleniumHandsOn1 {
     public static void main(String[] args) throws InterruptedException, NullPointerException {
-        String browser = args[0];
-        WebDriver driver = getDriver(browser);
-        WebElement myElement = driver.findElement(By.id("Element ID"));
-        driver.get("https://www.google.com.mx");
-        Thread.sleep(1000);
-        driver.quit();
+        // Define which browser to use
+        String browser = "firefox";
+
+        // Get correct driver for desire browser
+        WebDriver myDriver = WebDriverFactory.getDriver(browser);
+
+        // Get google home page
+        myDriver.get("https://www.google.com.mx");
+
+        // Wait some seconds
+        Thread.sleep(5000);
+
+        // Quit web driver
+        myDriver.quit();
     }
 }
 
--- src/test/java/com/qalabs/seleniumbasics/WebDriverFactory.java
+++ src/test/java/com/qalabs/seleniumbasics/WebDriverFactory.java
@@ -6,35 +6,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.io.File;
-import java.util.concurrent.TimeUnit;
 
-public class PageLoadTimeOutExample {
+public class WebDriverFactory {
+
     public static WebDriver getDriver(String browser) {
         File rootPath = new File("src/test/resources/lib-thirdparty/driversforwin");
-        if(browser.equals("chrome")) {
+        if(browser.toLowerCase().equals("chrome")) {
             File chromeFilePath = new File(rootPath, "chromedriver.exe");
             System.setProperty("webdriver.chrome.driver", chromeFilePath.getPath());
             return new ChromeDriver();
-        } else if (browser.equals("firefox")) {
-            File firefoxFilePath = new File(rootPath, "chromedriver.exe");
+        } else if (browser.toLowerCase().equals("firefox")) {
+            File firefoxFilePath = new File(rootPath, "geckodriver.exe");
             System.setProperty("webdriver.gecko.driver", firefoxFilePath.getPath());
             return new FirefoxDriver();
         } else {
             return null;
         }
     }
-    public static void main(String[] args) throws InterruptedException {
-        String browser = args[0];
-        WebDriver driver = getDriver(browser);
-
-        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
 
 
-        driver.get("https://www.google.com.mx");
-        driver.quit();
-    }
 }
-
-
-
-
